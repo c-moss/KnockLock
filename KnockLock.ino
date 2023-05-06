1836d4f00fb2c6e49f2010bb4c5b9538d47e01af
Campbell Moss <campbell.moss@gmail.com>
2023-05-06 16:29:50 +1200

    Reduce group interval again, and optimize unlock sequence

--- KnockLock.ino
+++ KnockLock.ino
@@ -6,7 +6,7 @@ const int KNOCK_CODE[] = {2, 3, 2};
 const int KNOCK_CODE_LENGTH = 3;  //length of knock code array
 const long INACTIVITY_TIMEOUT = 3000; //number of ms to wait before resetting state
 const long KNOCK_RECEIVED_DELAY = 100; //number of ms to wait after getting a knock so that we don't accidentally count it multiple times
-const long KNOCK_CODE_GROUP_DELAY = 700; //number of ms to wait before starting a new knock code group;
+const long KNOCK_CODE_GROUP_DELAY = 500; //number of ms to wait before starting a new knock code group;
 
 long knockTime = 0;
 int knocksReceived = 0;
@@ -24,7 +24,6 @@ void loop() {
       knockCodeProgress++;
       knocksReceived = 0;
       if (knockCodeProgress >= KNOCK_CODE_LENGTH) {
-        successTone();
         unlock();
       }
     } else {  // if the knock code group delay has elapsed without enough knocks, reset
@@ -54,12 +53,23 @@ boolean checkForKnock() {
   return knockVal > KNOCK_THRESHOLD;
 }
 
+// unlock and play success tone simultaneously
 void unlock() {
   reset();
   digitalWrite(LOCK_PIN, HIGH);
-  delay(500);
+  pinMode(BUZZER_PIN, OUTPUT);
+  tone(BUZZER_PIN, 500);
+  delay(200);
+  tone(BUZZER_PIN, 1000);
+  delay(200);
   digitalWrite(LOCK_PIN, LOW);
-  delay(1000);  //delay for a second so that the noise of the deadbolts unlocking isn't interpreted as a knock
+  tone(BUZZER_PIN, 1250);
+  delay(200);
+  tone(BUZZER_PIN, 1500);
+  delay(200);
+  noTone(BUZZER_PIN);
+  delay(200);
+  pinMode(BUZZER_PIN, INPUT);
 }
 
 void fail() {
@@ -72,21 +82,6 @@ void fail() {
 void reset() {
   knocksReceived = 0;
   knockCodeProgress = 0;
-}
-
-void successTone() {
-  pinMode(BUZZER_PIN, OUTPUT);
-  tone(BUZZER_PIN, 500);
-  delay(200);
-  tone(BUZZER_PIN, 1000);
-  delay(200);
-  tone(BUZZER_PIN, 1250);
-  delay(200);
-  tone(BUZZER_PIN, 1500);
-  delay(200);
-  noTone(BUZZER_PIN);
-  delay(200);
-  pinMode(BUZZER_PIN, INPUT);
 }
 
 void failTone() {
